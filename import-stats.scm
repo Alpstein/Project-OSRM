9d0bd08ba460b52a8e53b24e644086b066611469
Jens Thiele <karme@karme.de>
2013-07-24 18:05:06 +0200

    alist output

--- import-stats.scm
+++ import-stats.scm
@@ -13,30 +13,30 @@
            (if (not cmd)
              (cond [(#/Script started on (.*)/ line)
                     => (lambda (m)
-                         (set! stats (list 0 0))
-                         (set! script-run (list (m 1))))]
+                         (set! stats '((in-ways 0) (out-ways 0)))
+                         (set! script-run `((date ,(m 1)))))]
                    [(#/splited \d+\/(\d+) ways into (\d+) ways/ line)
                     => (lambda (m)
                          ;;#?=(m)
                          (assert stats)
-                         (inc! (car stats) (string->number (m 1)))
-                         (inc! (cadr stats) (string->number (m 2))))]
+                         (inc! (cadar stats) (string->number (m 1)))
+                         (inc! (cadadr stats) (string->number (m 2))))]
                    [(#/Command being timed: \"([^ ]+) ([^\"]+)\"/ line)
                     => (lambda (m)
                          (assert stats)
-                         (set! cmd (list (m 2) (m 1))))])
+                         (set! cmd `((cmd ,(m 1) ,(m 2)))))])
              (cond [(#/Elapsed \(wall clock\) time \(h:mm:ss or m:ss\): ((\d*):)?(\d+):(\d+(.\d+)?)/ line)
                     => (lambda (m)
-                         (push! cmd (+ (* (+ (* (or (and (m 2) (string->number (m 2))) 0) 60)
-                                             (string->number (m 3)))
-                                          60)
-                                       (string->number (m 4)))))]
+                         (push! cmd `(wall-clock ,(+ (* (+ (* (or (and (m 2) (string->number (m 2))) 0) 60)
+                                                           (string->number (m 3)))
+                                                        60)
+                                                     (string->number (m 4))))))]
                    [(#/Maximum resident set size \(kbytes\): (\d+)/ line)
                     => (lambda (m)
-                         (push! cmd (* (string->number (m 1)) 1024)))]
+                         (push! cmd `(max-rss ,(* (string->number (m 1)) 1024))))]
                    [(#/Major \(requiring I\/O\) page faults: (\d+)/ line)
                     => (lambda (m)
-                         (push! cmd (string->number (m 1))))]
+                         (push! cmd `(maj-faults ,(string->number (m 1)))))]
                    [(#/Exit status: / line)
                     (push! res (append script-run (reverse cmd) stats))
                     (set! cmd #f)])))
